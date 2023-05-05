d870819a2ca8877b8931ba3071358c1251cb13f0
pekepek <ishihata@33i.co.jp>
2023-05-05 18:17:16 +0900

    gem init

---
+++ lib/console/shogi.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "shogi/version"
+
+module Console
+  module Shogi
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/console/shogi/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Console
+  module Shogi
+    VERSION = "0.1.0"
+  end
+end
