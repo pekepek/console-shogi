1a0c87d5c0fb8e23aad3695ecab4a81798105a56
pekepek <ishihata@33i.co.jp>
2023-05-27 14:08:24 +0900

    PieceMover からカーソルの移動処理を削除

--- lib/console_shogi/piece_mover.rb
+++ lib/console_shogi/piece_mover.rb
@@ -2,58 +2,47 @@
 
 module ConsoleShogi
   class PieceMover
-    def initialize(board:, from:)
+    def initialize(board:, from:, to:)
       @board = board
-      @from_piece_index = from
+      @from = from
+      @to = to
     end
 
     def move!
-      return if target_piece.nil? || target_piece.none?
+      return if from_piece.nil? || from_piece.none?
 
-      while key = STDIN.getch
-        if key == "\e" && STDIN.getch == "["
-          key = STDIN.getch
+      return unless can_move?
 
-          TerminalOperator.move_cursor(key)
-        elsif key == "\r"
-          to_piece_index = TerminalOperator.squares_index
+      board.move_piece!(
+        from: {x: from[:x], y: from[:y]},
+        to: {x: to[:x], y: to[:y]}
+      )
 
-          return unless can_move?(piece: target_piece, to_piece_index: to_piece_index)
+      # TODO とりあえずここに実装してしまっている。整理したい
+      piece = board.fetch_piece(x: to[:x], y: to[:y])
+      return unless can_promote?(piece, from, to)
 
-          board.move_piece!(
-            from: {x: from_piece_index[:x], y: from_piece_index[:y]},
-            to: {x: to_piece_index[:x], y: to_piece_index[:y]}
-          )
-
-          # TODO とりあえずここに実装してしまっている。整理したい
-          piece = board.fetch_piece(x: to_piece_index[:x], y: to_piece_index[:y])
-          return unless can_promote?(piece, from_piece_index, to_piece_index)
-
-          board.promote_piece!(x: to_piece_index[:x], y: to_piece_index[:y]) if TerminalOperator.select_promotion
-
-          return
-        end
-      end
+      board.promote_piece!(x: to[:x], y: to[:y]) if TerminalOperator.select_promotion
     end
 
-    def can_move?(piece:, to_piece_index:)
-      return false unless board.within_range?(x: to_piece_index[:x], y: to_piece_index[:y])
+    def can_move?
+      return false unless board.within_range?(x: to[:x], y: to[:y])
 
-      diff = {x: to_piece_index[:x] - from_piece_index[:x], y: to_piece_index[:y] - from_piece_index[:y]}
+      diff = {x: to[:x] - from[:x], y: to[:y] - from[:y]}
 
-      return false if piece.moves.none? {|m| m[:x] == diff[:x] && m[:y] == diff[:y] }
+      return false if from_piece.moves.none? {|m| m[:x] == diff[:x] && m[:y] == diff[:y] }
 
-      to_piece = board.fetch_piece(x: to_piece_index[:x], y: to_piece_index[:y])
+      to_piece = board.fetch_piece(x: to[:x], y: to[:y])
 
-      return false if piece.player.teban == to_piece.player.teban
+      return false if from_piece.player.teban == to_piece.player.teban
 
-      return true unless piece.kaku? || piece.hisha? || piece.kyosha?
+      return true unless from_piece.kaku? || from_piece.hisha? || from_piece.kyosha?
 
       distance = (diff[:x].nonzero? || diff[:y]).abs
       element = [diff[:x] / distance, diff[:y] / distance]
 
       1.upto(distance - 1) do |d|
-        piece = board.fetch_piece(x: from_piece_index[:x] + element[0] * d, y: from_piece_index[:y] + element[1] * d)
+        piece = board.fetch_piece(x: from[:x] + element[0] * d, y: from[:y] + element[1] * d)
 
         return false unless piece.none?
       end
@@ -63,10 +52,10 @@ module ConsoleShogi
 
     private
 
-    attr_reader :board, :from_piece_index
+    attr_reader :board, :from, :to
 
-    def target_piece
-      @target_piece ||= board.fetch_piece(x: from_piece_index[:x], y: from_piece_index[:y])
+    def from_piece
+      @from_piece ||= board.fetch_piece(x: from[:x], y: from[:y])
     end
 
     def can_promote?(piece, from, to)
